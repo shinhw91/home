5207a188e8cd994b28bb2f32066628f22d1f38a4
신 혜원 <111713569+shinhw91@users.noreply.github.com>
2023-12-18 01:54:48 +0900

    Add files via upload

---
+++ T20231206/src/com/yedam/Employee.java
@@ -0,0 +1,33 @@
+package com.yedam;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+	// 속성(필드)
+	private String coNum;
+	private String name;
+	private String tel;
+	private String hireDate;
+	private int pay;
+	
+	// 생성자
+	Employee(String coNum, String name, String tel, String hireDate, int pay) {
+		this.coNum = coNum;
+		this.name = name;
+		this.tel = tel;
+		this.hireDate = hireDate;
+		this.pay = pay;
+	}
+	
+	void ShowList() {
+		System.out.printf("%s\t %s\t %s\t %d\n", coNum, name, tel, pay);
+	}
+	
+	void ShowInfo() {
+		System.out.printf("%s\t %s\t %s\n", coNum, name, hireDate);
+	}
+}
+
+// 클래스 : 대문자로 시작
+// 속성, 메소드 : 소문자로 시작
